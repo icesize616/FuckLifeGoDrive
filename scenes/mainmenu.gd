68ae0b532f1e133beca3cd3211259192caf04f44
icesize616 <ivanovlexa2002@gmail.com>
2021-08-06 03:11:14 +0500

    Network is work

--- scenes/mainmenu.gd
+++ scenes/mainmenu.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _ready():
-	pass # Replace with function body.
+	get_tree().connect("network_peer_connected",self,"_connected")
 
 
 func _on_Create_Host_pressed():
@@ -15,3 +15,9 @@ func _on_Connect_to_server_pressed():
 	var client = NetworkedMultiplayerENet.new()
 	client.create_client("127.0.0.1",8080)
 	get_tree().set_network_peer(client)
+
+func _connected(client_id):
+	Singlton.user_id = client_id
+	var game = preload("res://scenes/world.tscn").instance()
+	get_tree().get_root().add_child(game)
+	hide()
